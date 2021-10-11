4d6b005ffb718cc008dc236ed6796910613f717d
myshash101 <myshash101@hotmail.com>
2021-10-10 19:58:48 -0500

    Validaciones corrección función

---
+++ validaciones.js
@@ -0,0 +1,28 @@
+function validar_nombre_usuario(string){
+  
+}
+
+function validar_edad_usuario(edad){
+
+}
+
+function validar_contrasena(string){
+
+    //verifica si tiene caracteres de la A a la Z y del 0 al 9
+    var expresion = /^([a-zA-ZñÑ0-9_-]){1,30}$/;
+    if(expresion.test(string)==true){
+        if(string.length<6){
+            return false;
+        }
+        return true;
+    
+    }else{
+        return false;
+    }
+    
+}
+
+
+module.exports.validar_nombre_usuario = validar_nombre_usuario;
+module.exports.validar_contrasena = validar_contrasena;
+module.exports.validar_edad_usuario = validar_edad_usuario;
