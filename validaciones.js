20a5807e4d41194ab20391f71c87e34a823ddee8
joykep10 <mportillac@misena.edu.co>
2021-10-11 17:11:44 -0500

    probar validaciones

--- validaciones.js
+++ validaciones.js
@@ -1,6 +1,28 @@
+var nombre = document.getElementById("dato_nombre_usuario");
+var edad = document.getElementById("dato_edad_usuario");
+var contrasena = document.getElementById("dato_contrasena");
+var error = document.getElementById("error");
+error.style.color= 'red';
+var mensajesError=[];
+
+function todos(nombre,edad,contrasena){
+    validar_nombre_usuario(nombre);
+    validar_edad_usuario(edad); 
+    validar_contrasena(contrasena);
+   
+    error.innerHTML=mensajesError.join(' - ');
+    if(mensajesError.length == 1){ 
+        mensaje.innerHTML="";
+    }
+	if(mensajesError.length == 0){ 
+        mensaje.innerHTML="INGRESO DATOS FINALIZADO";
+    } 
+    return false;
+}
+
 function validar_nombre_usuario(string){
     //validar solo letras A a la Z y espacios, validar que no inicie ni termine con espacios, validar que cada letra despues del espacio sea mayuscula//
-    const expresiones = {
+    /*const expresiones = {
         nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
     }
     const campos = {
@@ -10,17 +32,38 @@ function validar_nombre_usuario(string){
         switch (e.target.name) {
             case "nombre":
                 validarCampo(expresiones.nombre, e.target, 'nombre');
-            break;  
-    
-function validar_edad_usuario(edad){
+            break;   */  
+}
 
+function validar_edad_usuario(edad){    
+    var expresionEdad = /^[0-9]/;
+    console.log('Enviando formulario....');
+    
+    if(edad.value=== null || edad.value===''){ 
+        mensajesError.push('No Ingresa la edad');
+    } 
+    else if(!expresionEdad.test(edad.value)){
+        mensajesError.push('No ingresa datos validos, solo numeros');
+    }
+    else if(edad.value<13 || edad.value>110){
+        mensajesError.push('Edad incorrecta ');
+    }
+    
 }
 
 function validar_contrasena(string){
 
     //verifica si tiene caracteres de la A a la Z y del 0 al 9
     var expresion = /^([a-zA-ZñÑ0-9_-]){1,30}$/;
-    if(expresion.test(string)==true){
+    
+    if(contrasena.value=== null || contrasena.value===''){ 
+        mensajesError.push('No Ingresa constraseña');
+    } 
+    else if(!expresion.test(contrasena.value)){
+        mensajesError.push('No ingresa datos validos, contraseña');
+    }
+    
+    /*if(expresion.test(string)==true){
         if(string.length<6){
             return false;
         }
@@ -28,7 +71,7 @@ function validar_contrasena(string){
     
     }else{
         return false;
-    }
+    }*/
     
 }
 
